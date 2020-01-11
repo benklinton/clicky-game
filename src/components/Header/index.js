88a845791df4be699b1ff38b680071cd16a706d2
benklinton <benklinton@gmail.com>
2020-01-11 16:00:56 -0700

    file setup

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// import './style.css';
+
+// function Header(props) {
+//     return (
+//         <header>
+//         <div className= "title">Harrison Ford Click Game</div>
+//         <div className= "scoreboard"> Score: {props.score}</div>
+//         </header>
+//     )
+// }
+
+// export default Header
