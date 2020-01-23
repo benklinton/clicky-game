cfb9f6f5884e7c47a870798380dbf77119e1a61a
benklinton <benklinton@gmail.com>
2020-01-23 15:00:15 -0700

    Updates

---
+++ precache-manifest.0bbd2d556af20590082794fc484c19ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614259f85e757a8ac6b851f9a72b72b0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "edc05a140d4b138287b8",
+    "url": "/clicky-game/static/css/main.d0190275.chunk.css"
+  },
+  {
+    "revision": "24174d323ac21f04f040",
+    "url": "/clicky-game/static/js/2.49212cf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.49212cf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edc05a140d4b138287b8",
+    "url": "/clicky-game/static/js/main.924d5593.chunk.js"
+  },
+  {
+    "revision": "0f9f9acf91bc4b9d055b",
+    "url": "/clicky-game/static/js/runtime-main.c289e6cb.js"
+  }
+]);
